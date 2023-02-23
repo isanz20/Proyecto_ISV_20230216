604f9e02b316dc74368f54fb72f60dabfde850d4
isanz20 <125271936+isanz20@users.noreply.github.com>
2023-02-23 14:21:52 +0100

    Añadida clase "Clase"

---
+++ src/paquete/Clase.java
@@ -0,0 +1,20 @@
+package paquete;
+
+public class Clase {
+	private String str;
+	
+	
+	public Clase(String str) {
+		this.str = str;
+	}
+
+	public String getStr() {
+		return str;
+	}
+
+	public void setStr(String str) {
+		this.str = str;
+	}
+	
+	
+}
--- src/paquete/Main.java
+++ src/paquete/Main.java
@@ -4,6 +4,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		System.out.println("Hola Mundo!");
+		Clase clase = new Clase("Hola!");
+		System.out.println(clase.getStr());
 	}
 
 }
